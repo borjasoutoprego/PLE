9c0cc8d4fc7d1ec560637f051c024d7b311dd7a9
borjasoutoprego <borjasoutoprego@gmail.com>
2023-02-15 21:06:19 +0100

    first tokens

--- p1/p1_base.py
+++ p1/p1_base.py
@@ -2,7 +2,7 @@ import sys
 
 from sly import Lexer
 
-# Se proporciona LogLexer como el analixador léxico de base que debéis completar.
+# Se proporciona LogLexer como el analizador léxico de base que debéis completar.
 # Podéis implementar lexers adicionales si decidís realizar un análisis léxico condicional.
 
 
@@ -11,7 +11,20 @@ class LogLexer(Lexer):
     Lexer base que debéis completar
     '''
 
-    tokens = {}
+    tokens = {FECHA, NOMBRE, SERVICIO, MENSAJE}
+
+    FECHA = r'[a-zA-Z]+[ |\t][0-9]+[ |\t][0-9:]+'
+    NOMBRE = r'[a-zA-Z0-9]+'
+    SERVICIO = r'sshd\[[0-9]+\]'
+    MENSAJE = r'Accepted password for|Failed password for|Invalid user|Failed password for invalid user' # gestionar demas tipos de eventos
+    # añadir mas tokens para el resto de campos del mensaje
+
+    ignore = r': ' # que ignore los dos puntos del campo ssh
+    ignore_space = r' '
+    ignore_newline = r'\r?\n'
+
+    def ignore_newline(self, t):
+        self.contador += 1
 
     def print_output(self):
         '''
