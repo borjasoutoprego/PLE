92faf20b48d85ae742f9d2cb643792809e38a54e
borjasoutoprego <borjasoutoprego@gmail.com>
2023-02-21 13:51:30 +0100

    init contador numero total de eventos

--- p1/p1_base.py
+++ p1/p1_base.py
@@ -13,17 +13,25 @@ class LogLexer(Lexer):
 
     tokens = {FECHA, NOMBRE, SERVICIO, MENSAJE}
 
-    FECHA = r'[a-zA-Z]+[ |\t][0-9]+[ |\t][0-9:]+'
+    FECHA = r'[a-zA-Z]+[ |\t][0-9]+[ |\t][0-9:]+' ## revisar espacios en blanco --> \s 
     NOMBRE = r'[a-zA-Z0-9]+'
     SERVICIO = r'sshd\[[0-9]+\]'
     MENSAJE = r'Accepted password for|Failed password for|Invalid user|Failed password for invalid user' # gestionar demas tipos de eventos
     # añadir mas tokens para el resto de campos del mensaje
 
+    ##################
+    # ¿¿¿¿¿Será util análisis lexico condicional para gestionar todas las partes del mensaje????????????????
+    ##################
+
+
     ignore = r': ' # que ignore los dos puntos del campo ssh
     ignore_space = r' '
     ignore_newline = r'\r?\n'
 
-    def ignore_newline(self, t):
+    def __init__(self):
+        self.contador = 1 # inicializado a 1 porque el nº de eventos es igual al nº de saltos de linea + 1
+
+    def ignore_newline(self, t): # se puede utilizar para contador del numero total de eventos? sumando 1 al final porque hay un salto de linea menos que numero de eventos
         self.contador += 1
 
     def print_output(self):
@@ -33,7 +41,7 @@ class LogLexer(Lexer):
         contadores indicados en el enunciado según el formato especificado.
         '''
 
-        pass
+        print('#contadores_generales\ntotal_eventos,'f'{self.contador}')
 
 
 # No debéis modificar el comportamiento de esta sección
