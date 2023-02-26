5af2189b921bb2dc65a9ff19260537de45b8d500
Nina López Laudenbach <nina.laudenbach@udc.es>
2023-02-26 21:29:06 +0100

    final code without errors

--- p1/p1_base.py
+++ p1/p1_base.py
@@ -14,16 +14,14 @@ class LogLexer(Lexer):
     Lexer base que debéis completar
     '''
 
-    tokens = {MONTH, DAY, HOUR, NAME, SERVICE}
+    tokens = {MONTH, DAY, HOUR, MACHINE, SERVICE}
     ignore = ' \t:'
-
-    # tokens separados para clase de IP
 
     MONTH = r'[A-Z][a-z]{2}'
     HOUR = r'[0-9]+[:][0-9]+[:][0-9]+'
     DAY = r'[0-9]{1,2}'
     SERVICE = r'sshd\[[0-9]+\]'
-    NAME = r'[a-zA-Z0-9]+'
+    MACHINE = r'[a-zA-Z0-9]+'
 
     ignore_newline = r'\r?\n'
 
@@ -39,10 +37,18 @@ class LogLexer(Lexer):
         self.failed = 0
         self.invalid = 0
         self.temp_msg = ''
+        self.temp_machine = ''
         self.dictMachine = dict()
-        self.dictIP = dict()
+        self.dictAcc_Mach = dict()
+        self.dictFailed_Mach = dict()
+        self.dictInvalid_Mach = dict()
+        self.dictAcc_User = dict()
+        self.dictFailed_User = dict()
+        self.dictInvalid_User = dict()
         self.dictUser = dict()
-        self.dictClass = dict()
+        self.dictAcc_IP = dict()
+        self.dictFailed_IP = dict()
+        self.dictInvalid_IP = dict()
         self.others = 0
 
     def MONTH(self, t):
@@ -52,11 +58,11 @@ class LogLexer(Lexer):
 
     def DAY(self, t):
         if int(t.value) < 16:
-            self.half = '1'
+            self.half = 'primera'
         else:
-            self.half = '2'
+            self.half = 'segunda'
 
-        key = self.month + self.half
+        key = self.month + ',' + self.half
 
         if key not in self.dictDate:
             self.dictDate[key] = 1
@@ -64,6 +70,16 @@ class LogLexer(Lexer):
             self.dictDate[key] += 1
 
         return t
+    
+    def MACHINE(self, t):
+        # Eventos por máquina
+        k = str(t.value)
+        if k not in self.dictMachine:
+            self.dictMachine[k] = 1
+        else:
+            self.dictMachine[k] += 1
+
+        self.temp_machine = t.value
 
     def HOUR(self, t):
         hour = int(str(t.value[0]) + str(t.value[1]))
@@ -85,52 +101,99 @@ class LogLexer(Lexer):
         contadores indicados en el enunciado según el formato especificado.
         '''
 
-        print('#contadores_generales\ntotal_eventos,'f'{self.counter}','\ntotal_aceptados,'f'{self.acc}','\ntotal_fallidos,'f'{self.failed}','\ntotal_no_autorizados,',f'{self.invalid}', '\ntotal_otros,'f'{self.others}')
-        print('#eventos_por_fecha')
+        print(f'#contadores_generales\ntotal_eventos,{self.counter}\ntotal_aceptados,{self.acc}\ntotal_fallidos,{self.failed}\ntotal_no_autorizados,{self.invalid}\ntotal_otros,{self.others}')
+
+        print('#eventos_por_fecha') 
         for keys, values in self.dictDate.items():
-            print(keys, values)
-        print('#eventos_por_hora\nmanana,'f'{self.morning}','\ntarde,',f'{self.aft}','\nnoche,'f'{self.night}')
-        print('#eventos_por_maquina')
+            print(keys, ',', values, sep='')
+
+        print(f'#eventos_por_hora\nmanana,{self.morning}\ntarde,{self.aft}\nnoche,{self.night}') 
+
+        print('#eventos_por_maquina') 
         for keys, values in self.dictMachine.items():
-            print(keys, values)
-        print('#eventos_por_maquina segun mensaje ######')
-        for keys, values in self.dictIP.items():
-            print(keys, values)
-        print('#eventos por usuario segun mensaje ######')
-        for keys, values in self.dictUser.items():
-            print(keys, values)
-        print('#eventos_por_ip segun mensaje ######')
-        for keys, values in self.dictClass.items():
-            print(keys, values)
-        
+            print(keys, ',', values, sep='')
+
+        print('#eventos_aceptados_por_maquina')
+        for keys, values in self.dictAcc_Mach.items():
+            print(keys, ',', values, sep='')
+
+        print('#eventos_aceptados_por_usuario')
+        for keys, values in self.dictAcc_User.items():
+            print(keys, ',', values, sep='')
+
+        print('#eventos_aceptados_por_ip') 
+        for keys, values in self.dictAcc_IP.items():
+             print(keys, ',', values, sep='')
+
+        print('#eventos_fallidos_por_maquina') 
+        for keys, values in self.dictFailed_Mach.items():
+            print(keys, ',', values, sep='')
+
+        print('#eventos_fallidos_por_usuario') 
+        for keys, values in self.dictFailed_User.items():
+             print(keys, ',', values, sep='')
+
+        print('#eventos_fallidos_por_ip') 
+        for keys, values in self.dictFailed_IP.items():
+              print(keys, ',', values, sep='')
+
+        print('#eventos_no_autorizados_por_maquina') 
+        for keys, values in self.dictInvalid_Mach.items():
+            print(keys, ',', values, sep='')
+
+        print('#eventos_no_autorizados_por_usuario') 
+        for keys, values in self.dictInvalid_User.items():
+             print(keys, ',', values, sep='')
+
+        print('#eventos_no_autorizados_por_ip') 
+        for keys, values in self.dictInvalid_IP.items():
+             print(keys, ',', values, sep='')     
 
 class MessageLexer(Lexer):
-    tokens = {MESSAGE, OTHERS, USER, IP, END}
-    ignore = r' \t:\[\];=\(\)><\.\,\*\@\+\#!\$\'\/\{\}~%^&`'
+    tokens = {MESSAGE, OTHERS, USER, IP, END, FROM, NEWLINE}
+    ignore = r' \t'
 
-    MESSAGE = r'Accepted\spassword\sfor|Failed\spassword\sfor\sinvalid\suser|Invalid\suser|Failed\spassword\sfor'  
-    OTHERS = r'Dis.+|Rec.+|pam.+|error.+|Conn.+'
-    # IP = r'[0-9]+[\.][0-9]+[\.][0-9]+[\.][0-9]+'
+    FROM = r'from'
+    MESSAGE = r'(?i):\s(Accepted\spassword\sfor|Failed\spassword\sfor\sinvalid\suser|Invalid\suser|Failed\spassword\sfor)\b'  
+    OTHERS = r':\s.+'
     IP = r'((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
-    USER = r'[a-zA-Z0-9\-\_]+'
+    NEWLINE = r'\r?\n'
     END =   r'port.+'
+    USER = r'[^ \t]+'
 
-    ignore_newline = r'\r?\n'
-
-    def ignore_newline(self, t):
+    def NEWLINE(self, t):
         self.begin(LogLexer)
 
     def MESSAGE(self, t):
         # Total de mensajes identificados
-        if t.value == 'Accepted password for':
+        if t.value == ': Accepted password for':
             self.acc += 1 
-            self.temp_msg = 'acc'
-        elif t.value == 'Failed password for':
+            self.temp_msg = 'accepted'
+
+            key = self.temp_machine
+            if key not in self.dictAcc_Mach:
+                self.dictAcc_Mach[key] = 1
+            else:
+                self.dictAcc_Mach[key] += 1
+            
+        elif t.value == ': Failed password for':
             self.failed += 1
             self.temp_msg = 'failed'
+
+            key = self.temp_machine
+            if key not in self.dictFailed_Mach:
+                self.dictFailed_Mach[key] = 1
+            else:
+                self.dictFailed_Mach[key] += 1
         else:
             self.invalid += 1
             self.temp_msg = 'invalid'
+
+            key = self.temp_machine
+            if key not in self.dictInvalid_Mach:
+                self.dictInvalid_Mach[key] = 1
+            else:
+                self.dictInvalid_Mach[key] += 1
 
         return t 
 
@@ -139,20 +202,6 @@ class MessageLexer(Lexer):
         return t
 
     def IP(self, t):
-        # Número de eventos por máquina (total)
-        k = str(t.value)
-        if k not in self.dictMachine:
-            self.dictMachine[k] = 1
-        else:
-            self.dictMachine[k] += 1
-
-        # Número de eventos por máquina según mensaje
-        key = self.temp_msg + str(t.value)
-        if key not in self.dictIP:
-            self.dictIP[key] = 1
-        else:
-            self.dictIP[key] += 1
-
         # Contadores según tipo de IP
         if t.value[1] == '.':
             typeIP = int(str(t.value[0]))
@@ -163,9 +212,10 @@ class MessageLexer(Lexer):
 
         if typeIP < 128: # tipo A
             if typeIP == 10:
-                key1 = self.temp_msg + 'A' + 'private'
+                key1 = 'clase_a,' + 'privada'
             else:
-                key1 = self.temp_msg + 'A' + 'public'
+                key1 = 'clase_a,' + 'publica'
+
         elif 127 < typeIP < 192: # tipo B
             if t.value[5] == '.':
                 IP2 = int(str(t.value[4]))
@@ -175,9 +225,10 @@ class MessageLexer(Lexer):
                 IP2 = int(str(t.value[4]) + str(t.value[5]) + str(t.value[6]))
                 
             if typeIP == 172 and 15 < IP2 < 32:
-                key1 = self.temp_msg + 'B' + 'private'
+                key1 = 'clase_b,' + 'privada'
             else:
-                key1 = self.temp_msg + 'B' + 'public'
+                key1 = 'clase_b,' + 'publica'
+
         else: # tipo C
             if t.value[5] == '.':
                 IP2 = int(str(t.value[4]))
@@ -187,25 +238,47 @@ class MessageLexer(Lexer):
                 IP2 = int(str(t.value[4]) + str(t.value[5]) + str(t.value[6])) 
 
             if typeIP == 192 and IP2 == 168:
-                key1 = self.temp_msg + 'C' + 'private'
+                key1 = 'clase_c,' + 'privada'
             else:
-                key1 = self.temp_msg + 'C' + 'public'
+                key1 = 'clase_c,' + 'publica'
 
-        if key1 not in self.dictClass:
-            self.dictClass[key1] = 1
+        if self.temp_msg == 'accepted':
+            if key1 not in self.dictAcc_IP:
+                self.dictAcc_IP[key1] = 1
+            else:
+                self.dictAcc_IP[key1] += 1
+        elif self.temp_msg == 'failed':
+            if key1 not in self.dictFailed_IP:
+                self.dictFailed_IP[key1] = 1
+            else:
+                self.dictFailed_IP[key1] += 1
         else:
-            self.dictClass[key1] += 1
+            if key1 not in self.dictInvalid_IP:
+                self.dictInvalid_IP[key1] = 1
+            else:
+                self.dictInvalid_IP[key1] += 1
 
         return t
 
     def USER(self, t):
-        key = self.temp_msg + str(t.value)
-        if key not in self.dictUser:
-            self.dictUser[key] = 1
-        else:
-            self.dictUser[key] += 1
+        key = t.value
 
-        return t 
+        if self.temp_msg == 'accepted':
+            if key not in self.dictAcc_User:
+                self.dictAcc_User[key] = 1
+            else:
+                self.dictAcc_User[key] += 1
+            
+        elif self.temp_msg == 'failed':
+            if key not in self.dictFailed_User:
+                self.dictFailed_User[key] = 1
+            else:
+                self.dictFailed_User[key] += 1
+        else:
+            if key not in self.dictInvalid_User:
+                self.dictInvalid_User[key] = 1
+            else:
+                self.dictInvalid_User[key] += 1
 
 # No debéis modificar el comportamiento de esta sección
 if __name__ == '__main__':
@@ -224,16 +297,3 @@ if __name__ == '__main__':
     if text:
         list(lexer.tokenize(text, lineno=0))
         lexer.print_output()
-
-    
-    """ lexer = LogLexer()
-
-    while True:
-        try:
-            text = input(' > ')
-        except EOFError:
-            break
-        if text:
-            tokens = list(lexer.tokenize(text))
-            for t in tokens:
-                print(t) """
