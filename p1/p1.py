cac50a38e5c8227e00ca029357bcb201789b4662
borjasoutoprego <borjasoutoprego@gmail.com>
2023-02-27 12:29:45 +0100

    added functions generateDict and printDict

--- p1/p1.py
+++ p1/p1.py
@@ -8,10 +8,20 @@ from sly import Lexer
 # Se proporciona LogLexer como el analizador léxico de base que debéis completar.
 # Podéis implementar lexers adicionales si decidís realizar un análisis léxico condicional.
 
+def generateDict(dictName, key):
+    if key not in dictName:
+        dictName[key] = 1
+    else:
+        dictName[key] += 1
+
+def printDict(dictName):
+    for keys, values in dictName.items():
+        print(keys, ',', values, sep='')
 
 class LogLexer(Lexer):
     '''
     Lexer base que debéis completar
+    Utilizado para el análisis de la parte fija de los logs (hasta servicio)
     '''
 
     tokens = {MONTH, DAY, HOUR, MACHINE, SERVICE}
@@ -57,31 +67,26 @@ class LogLexer(Lexer):
         return t 
 
     def DAY(self, t):
-        if int(t.value) < 16:
+        # comprobación de la quincena
+        if int(t.value) < 16: 
             self.half = 'primera'
         else:
             self.half = 'segunda'
 
         key = self.month + ',' + self.half
-
-        if key not in self.dictDate:
-            self.dictDate[key] = 1
-        else:
-            self.dictDate[key] += 1
+        generateDict(self.dictDate, key)
 
         return t
     
     def MACHINE(self, t):
-        # Eventos por máquina
-        k = str(t.value)
-        if k not in self.dictMachine:
-            self.dictMachine[k] = 1
-        else:
-            self.dictMachine[k] += 1
+        # eventos por máquina
+        key = str(t.value)
+        generateDict(self.dictMachine, key)
 
         self.temp_machine = t.value
 
     def HOUR(self, t):
+        # comprobación de la franja horaria
         hour = int(str(t.value[0]) + str(t.value[1]))
         if 8 <= hour < 16:
             self.morning += 1
@@ -92,6 +97,7 @@ class LogLexer(Lexer):
         return t
 
     def SERVICE(self, t):
+        # llamada de arranque al segundo lexer
         self.begin(MessageLexer)
 
     def print_output(self):
@@ -104,52 +110,45 @@ class LogLexer(Lexer):
         print(f'#contadores_generales\ntotal_eventos,{self.counter}\ntotal_aceptados,{self.acc}\ntotal_fallidos,{self.failed}\ntotal_no_autorizados,{self.invalid}\ntotal_otros,{self.others}')
 
         print('#eventos_por_fecha') 
-        for keys, values in self.dictDate.items():
-            print(keys, ',', values, sep='')
+        printDict(self.dictDate)
 
         print(f'#eventos_por_hora\nmanana,{self.morning}\ntarde,{self.aft}\nnoche,{self.night}') 
 
         print('#eventos_por_maquina') 
-        for keys, values in self.dictMachine.items():
-            print(keys, ',', values, sep='')
+        printDict(self.dictMachine)
 
         print('#eventos_aceptados_por_maquina')
-        for keys, values in self.dictAcc_Mach.items():
-            print(keys, ',', values, sep='')
+        printDict(self.dictAcc_Mach)
 
         print('#eventos_aceptados_por_usuario')
-        for keys, values in self.dictAcc_User.items():
-            print(keys, ',', values, sep='')
+        printDict(self.dictAcc_User)
 
         print('#eventos_aceptados_por_ip') 
-        for keys, values in self.dictAcc_IP.items():
-             print(keys, ',', values, sep='')
+        printDict(self.dictAcc_IP)
 
         print('#eventos_fallidos_por_maquina') 
-        for keys, values in self.dictFailed_Mach.items():
-            print(keys, ',', values, sep='')
+        printDict(self.dictFailed_Mach)
 
         print('#eventos_fallidos_por_usuario') 
-        for keys, values in self.dictFailed_User.items():
-             print(keys, ',', values, sep='')
+        printDict(self.dictFailed_User)
 
         print('#eventos_fallidos_por_ip') 
-        for keys, values in self.dictFailed_IP.items():
-              print(keys, ',', values, sep='')
+        printDict(self.dictFailed_IP)
 
         print('#eventos_no_autorizados_por_maquina') 
-        for keys, values in self.dictInvalid_Mach.items():
-            print(keys, ',', values, sep='')
+        printDict(self.dictInvalid_Mach)
 
         print('#eventos_no_autorizados_por_usuario') 
-        for keys, values in self.dictInvalid_User.items():
-             print(keys, ',', values, sep='')
+        printDict(self.dictInvalid_User)
 
         print('#eventos_no_autorizados_por_ip') 
-        for keys, values in self.dictInvalid_IP.items():
-             print(keys, ',', values, sep='')     
+        printDict(self.dictInvalid_IP)  
 
 class MessageLexer(Lexer):
+    """
+    Lexer para el análisis de la parte del mensaje de los logs   
+    """
+
     tokens = {MESSAGE, OTHERS, USER, IP, END, FROM, NEWLINE}
     ignore = r' \t'
 
@@ -162,6 +161,7 @@ class MessageLexer(Lexer):
     USER = r'[^ \t]+'
 
     def NEWLINE(self, t):
+        # llamada de arranque al primer lexer
         self.begin(LogLexer)
 
     def MESSAGE(self, t):
@@ -171,29 +171,21 @@ class MessageLexer(Lexer):
             self.temp_msg = 'accepted'
 
             key = self.temp_machine
-            if key not in self.dictAcc_Mach:
-                self.dictAcc_Mach[key] = 1
-            else:
-                self.dictAcc_Mach[key] += 1
-            
+            generateDict(self.dictAcc_Mach, key)
+
         elif t.value == ': Failed password for':
             self.failed += 1
             self.temp_msg = 'failed'
 
             key = self.temp_machine
-            if key not in self.dictFailed_Mach:
-                self.dictFailed_Mach[key] = 1
-            else:
-                self.dictFailed_Mach[key] += 1
+            generateDict(self.dictFailed_Mach, key)
+            
         else:
             self.invalid += 1
             self.temp_msg = 'invalid'
 
             key = self.temp_machine
-            if key not in self.dictInvalid_Mach:
-                self.dictInvalid_Mach[key] = 1
-            else:
-                self.dictInvalid_Mach[key] += 1
+            generateDict(self.dictInvalid_Mach, key)
 
         return t 
 
@@ -212,9 +204,9 @@ class MessageLexer(Lexer):
 
         if typeIP < 128: # tipo A
             if typeIP == 10:
-                key1 = 'clase_a,' + 'privada'
+                key = 'clase_a,' + 'privada'
             else:
-                key1 = 'clase_a,' + 'publica'
+                key = 'clase_a,' + 'publica'
 
         elif 127 < typeIP < 192: # tipo B
             if t.value[5] == '.':
@@ -225,9 +217,9 @@ class MessageLexer(Lexer):
                 IP2 = int(str(t.value[4]) + str(t.value[5]) + str(t.value[6]))
                 
             if typeIP == 172 and 15 < IP2 < 32:
-                key1 = 'clase_b,' + 'privada'
+                key = 'clase_b,' + 'privada'
             else:
-                key1 = 'clase_b,' + 'publica'
+                key = 'clase_b,' + 'publica'
 
         else: # tipo C
             if t.value[5] == '.':
@@ -238,47 +230,27 @@ class MessageLexer(Lexer):
                 IP2 = int(str(t.value[4]) + str(t.value[5]) + str(t.value[6])) 
 
             if typeIP == 192 and IP2 == 168:
-                key1 = 'clase_c,' + 'privada'
+                key = 'clase_c,' + 'privada'
             else:
-                key1 = 'clase_c,' + 'publica'
+                key = 'clase_c,' + 'publica'
 
         if self.temp_msg == 'accepted':
-            if key1 not in self.dictAcc_IP:
-                self.dictAcc_IP[key1] = 1
-            else:
-                self.dictAcc_IP[key1] += 1
+            generateDict(self.dictAcc_IP, key)
         elif self.temp_msg == 'failed':
-            if key1 not in self.dictFailed_IP:
-                self.dictFailed_IP[key1] = 1
-            else:
-                self.dictFailed_IP[key1] += 1
+            generateDict(self.dictFailed_IP, key)
         else:
-            if key1 not in self.dictInvalid_IP:
-                self.dictInvalid_IP[key1] = 1
-            else:
-                self.dictInvalid_IP[key1] += 1
+            generateDict(self.dictInvalid_IP, key)
 
         return t
 
     def USER(self, t):
         key = t.value
-
         if self.temp_msg == 'accepted':
-            if key not in self.dictAcc_User:
-                self.dictAcc_User[key] = 1
-            else:
-                self.dictAcc_User[key] += 1
-            
+            generateDict(self.dictAcc_User, key)           
         elif self.temp_msg == 'failed':
-            if key not in self.dictFailed_User:
-                self.dictFailed_User[key] = 1
-            else:
-                self.dictFailed_User[key] += 1
+            generateDict(self.dictFailed_User, key)
         else:
-            if key not in self.dictInvalid_User:
-                self.dictInvalid_User[key] = 1
-            else:
-                self.dictInvalid_User[key] += 1
+            generateDict(self.dictInvalid_User, key)
 
 # No debéis modificar el comportamiento de esta sección
 if __name__ == '__main__':
