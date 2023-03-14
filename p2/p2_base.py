8913c3ec5330937003f4c99d4095af980be9dcf6
borjasoutoprego <borjasoutoprego@gmail.com>
2023-03-14 21:43:00 +0100

    add lexer and parser, error control, lat and lon

--- p2/p2_base.py
+++ p2/p2_base.py
@@ -2,19 +2,28 @@
 @author: Nina López Laudenbach (nina.laudenbach), Borja Souto Prego (borja.souto)
 """
 import sys
-
+import json
 from sly import Lexer, Parser
 
 ## Definir tokens para todas las etiquetas y no usar OTHERS
 ## ¿Como controlar la etiqueta que falta?
 
 class GPXLexer(Lexer):
-    TOKENS = {ELEV_OPEN, ELEV_CLOSE, HR_OPEN, HR_CLOSE, CAD_OPEN, CAD_CLOSE, TEMP_OPEN, 
-    TEMP_CLOSE, NAME_OPEN, NAME_CLOSE, TYPE_OPEN, TYPE_CLOSE, LATITUDE, LONGITUDE, DATE_OPEN, 
-    DATE_CLOSE, TRKSEG_OPEN, TRKSEG_CLOSE, TEXT_OPEN, TEXT_CLOSE, OTHERS, VALUE}
+    tokens = {GPX_OPEN, GPX_CLOSE, TRK_OPEN, TRK_CLOSE, ELEV_OPEN, ELEV_CLOSE, HR_OPEN, HR_CLOSE, CAD_OPEN, CAD_CLOSE, TEMP_OPEN, 
+    TEMP_CLOSE, NAME_OPEN, NAME_CLOSE, TYPE_OPEN, TYPE_CLOSE, TIME_OPEN, 
+    TIME_CLOSE, EXTENSIONS_OPEN, EXTENSIONS_CLOSE, TPE_OPEN, TPE_CLOSE, TRKSEG_OPEN, TRKSEG_CLOSE, 
+    TRKPT_OPEN, TRKPT_CLOSE, DATE, FLOAT, INT, STRING}
 
     ignore = ' \t'
+    ignore_newline = r'\r?\n'
+    ignore_header = r'<\?xml.*?\?>'
 
+    #HEADER = r'<\?xml.*?\?>'
+    GPX_OPEN = r'<gpx(.|\s)*?>'
+    ignore_metadata = '<metadata>([\t?\n].*?)+</metadata>'
+    GPX_CLOSE = r'</gpx>'
+    TRK_OPEN = r'<trk>'
+    TRK_CLOSE = r'</trk>'
     ELEV_OPEN = r'<ele>'
     ELEV_CLOSE = r'</ele>'
     HR_OPEN = r'<ns3:hr>'
@@ -27,133 +36,164 @@ class GPXLexer(Lexer):
     NAME_CLOSE = r'</name>'
     TYPE_OPEN = r'<type>'
     TYPE_CLOSE = r'</type>'
-    LATITUDE = r'lat="\.+"'
-    LONGITUDE = r'lon="\.+"'
-    DATE_OPEN = r'<time>'
-    DATE_CLOSE = r'</time>'
+    TIME_OPEN = r'<time>'
+    TIME_CLOSE = r'</time>'
+    EXTENSIONS_OPEN = r'<extensions>'
+    EXTENSIONS_CLOSE = r'</extensions>'
+    TPE_OPEN = r'<ns3:TrackPointExtension>'
+    TPE_CLOSE = r'</ns3:TrackPointExtension>'
     TRKSEG_OPEN = r'<trkseg>'
     TRKSEG_CLOSE = r'</trkseg>'
-    TEXT_OPEN = r'<text>'
-    TEXT_CLOSE = r'</text>'
-    OTHERS = r'<.+>|<.*|.*>'
-    VALUE = r'.+'
+    TRKPT_OPEN = r'<trkpt.+?>'
+    TRKPT_CLOSE = r'</trkpt>'
+    DATE = r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z'
+    FLOAT = r'\d+\.\d+'
+    INT = r'\d+'
+    STRING = r'[a-zA-Z_]+\s*[a-zA-Z_]*'
 
-    ignore_newline = r'(\r?\n)+'
+    def FLOAT(self, t):
+        t.value = float(t.value)
+        return t
+
+    def INT(self, t):
+        t.value = int(t.value)
+        return t
+
+    def STRING(self, t):
+        t.value = str(t.value)
+        return t
+
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    def error(self, t):
+        t.type = 'error'
+        t.value = t.value[0]
+        self.index += 1
+        return t
     
-
 class GPXParser(Parser):
     tokens = GPXLexer.tokens
 
     def __init__(self):
-        self.names = {}
         self.elevation = []
         self.heart_rate = []
         self.cadence = []
         self.temperature = []
-
-    @_('ELEV_OPEN VALUE ELEV_CLOSE')
-    def elevation(self, p):
-        try:
-            self.elevation.append(float(p.VALUE))
-            return p.VALUE
-        except ValueError:
-            print(f"Valor de elevación no válido ('{p.VALUE}') en la línea {self.lineno}")
-    
-    @_('HR_OPEN VALUE HR_CLOSE')
-    def hr(self, p):
-        try:
-            if int(p.VALUE) <= 0:
-                print(f"Valor de cadencia no válido ('{p.VALUE}') en la línea {self.lineno}")
-            else:
-                self.heart_rate.append(int(p.VALUE))
-                return p.VALUE
-        except ValueError:
-            print(f"Valor de cadencia no válido ('{p.VALUE}') en la línea {self.lineno}") 
-
-    @_('CAD_OPEN VALUE CAD_CLOSE')
-    def cad(self, p):
-        try:
-            if int(p.VALUE) <= 0:
-                print(f"Valor de cadencia no válido ('{p.VALUE}') en la línea {self.lineno}")
-            else:
-                self.cadence.append(int(p.VALUE))
-                return p.VALUE
-        except ValueError:
-            print(f"Valor de cadencia no válido ('{p.VALUE}') en la línea {self.lineno}")
-
-    @_('TEMP_OPEN VALUE TEMP_CLOSE')
-    def temp(self, p):
-        try:
-            self.temperature.append(float(p.VALUE))
-            return p.VALUE
-        except ValueError:
-            print(f"Valor de temperatura no válido ('{p.VALUE}') en la línea {self.lineno}")
-
-    @_('OTHERS temp hr cad OTHERS')
-    def tpe(self, p):
-        return p[1], p[2], p[3]
-
-    @_('OTHERS tpe OTHERS')
-    def extensions(self, p):
-        return p.tpe 
-
-    @_('TIME_OPEN VALUE TIME_CLOSE')
-    def time(self, p): ####### revisar si puede haber errores???
-        return p.VALUE
-
-    @_('OTHERS LATITUDE LONGITUDE OTHERS')
-    def coords(self, p):
-        return p.LATITUDE, p.LONGITUDE
-
-    @_('coords elevation time extensions OTHERS')
-    def trkpt(self, p):
-        return p.coords, p.elevation, p.time, p.extensions
-
-    @_('NAME_OPEN VALUE NAME_CLOSE')
-    def name(self, p):
-        return p.VALUE
-
-    @_('TYPE_OPEN VALUE TYPE_CLOSE')
-    def type(self, p):
-        return p.VALUE
-
-    @_('TRKSEG_OPEN trkpt')
-    def trkseg(self, p):
-        return p.trkpt
-    
-    @_('trkseg trkpt')
-    def trkseg(self, p):
-        return p.trkseg, p.trkpt
-    
-    @_('trkseg TRKSEG_CLOSE')
-    def trkseg(self, p):
-        return list(p.trkseg)
-
-    @_('OTHERS name type trkseg OTHERS')
-    def trk(self, p):
-        return p.name, p.type, p.trkseg
-
-    @_('TEXT_OPEN VALUE TEXT_CLOSE')
-    def text(self, p):
-        pass
-
-    @_('OTHERS text OTHERS')
-    def link(self, p):
-        pass
-
-    @_('OTHERS link time OTHERS')
-    def metadata(self, p):
-        pass
-
-    @_('OTHERS metadata trk OTHERS')
+        
+    @_('GPX_OPEN TRK_OPEN trk_content TRK_CLOSE GPX_CLOSE')
     def gpx(self, p):
-        return p.trk
+        return p.trk_content
 
-    # funcion para manejar ausencia de etiquetas de apertura y cierre
-    @_('error')
-    def error(self, p):
-        print(f"Error de sintaxis en la línea {self.lineno}")
+    @_('trk_content trk_item')
+    def trk_content(self, p):
+        p.trk_content.append(p.trk_item)
+        return p.trk_content
 
+    @_('trk_item')
+    def trk_content(self, p):
+        return [p.trk_item]
+
+    @_('NAME_OPEN STRING NAME_CLOSE')
+    def trk_item(self, p):
+        return p.STRING
+
+    @_('TYPE_OPEN STRING TYPE_CLOSE')
+    def trk_item(self, p):
+        return p.STRING
+
+    @_('TRKSEG_OPEN trkpoints TRKSEG_CLOSE')
+    def trk_item(self, p):
+        return p.trkpoints
+
+    @_('trkpoints trkpt')
+    def trkpoints(self, p):
+        p.trkpoints.append(p.trkpt)
+        return p.trkpoints
+
+    @_('trkpt')
+    def trkpoints(self, p):
+        return [p.trkpt]
+
+    @_('TRKPT_OPEN trkpt_content TRKPT_CLOSE')
+    def trkpt(self, p):
+        coords = p.TRKPT_OPEN.split('"')
+        p.trkpt_content.append(coords[1])
+        p.trkpt_content.append(coords[3])
+        return p.trkpt_content 
+
+    @_('trkpt_content trkpt_item')
+    def trkpt_content(self, p):
+        p.trkpt_content.append(p.trkpt_item)
+        return p.trkpt_content
+
+    @_('trkpt_item')
+    def trkpt_content(self, p):
+        return [p.trkpt_item]
+
+    @_('ELEV_OPEN FLOAT ELEV_CLOSE')
+    def trkpt_item(self, p):
+        self.elevation.append(p.FLOAT)
+        return p.FLOAT
+
+    @_('ELEV_OPEN INT ELEV_CLOSE')
+    def trkpt_item(self, p):
+        self.elevation.append(p.INT)
+        return p.INT
+
+    @_('TIME_OPEN DATE TIME_CLOSE')
+    def trkpt_item(self, p):
+        return p.DATE
+
+    @_('EXTENSIONS_OPEN TPE_OPEN tpe_content TPE_CLOSE EXTENSIONS_CLOSE')
+    def trkpt_item(self, p):
+        return p.tpe_content
+
+    @_('tpe_content tpe_item')
+    def tpe_content(self, p):
+        p.tpe_content.append(p.tpe_item)
+        return p.tpe_content
+
+    @_('tpe_item')
+    def tpe_content(self, p):
+        return [p.tpe_item]
+
+    @_('TEMP_OPEN FLOAT TEMP_CLOSE')
+    def tpe_item(self, p):
+        self.temperature.append(p.FLOAT)
+        return p.FLOAT
+
+    @_('TEMP_OPEN INT TEMP_CLOSE')
+    def tpe_item(self, p):
+        self.temperature.append(p.INT)
+        return p.INT
+
+    @_('TEMP_OPEN error TEMP_CLOSE')
+    def tpe_item(self, p):
+        print(f"Valor de temperatura no válido ('{p.error.value}') en la línea {p.error.lineno}")
+
+    @_('HR_OPEN INT HR_CLOSE')
+    def tpe_item(self, p):
+        self.heart_rate.append(p.INT)
+        return p.INT
+    
+    @_('HR_OPEN error HR_CLOSE')
+    def tpe_item(self, p):
+        print(f"Valor de ritmo cardíaco no válido ('{p.error.value}') en la línea {p.error.lineno}")
+
+    @_('CAD_OPEN INT CAD_CLOSE')
+    def tpe_item(self, p):
+        self.cadence.append(p.INT)
+        return p.INT   
+
+    @_('CAD_OPEN error CAD_CLOSE')
+    def tpe_item(self, p):
+        print(f"Valor de cadencia no válido ('{p.error.value}') en la línea {p.error.lineno}")
+
+# json.dumps()
+# ensure_ascii = False
+# indent = 2
+# cls = EnhancedJSONEncoder
 
     # No debéis modificar el comportamiento de esta sección
 if __name__ == '__main__':
